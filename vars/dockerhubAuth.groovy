51a46690126635610454fe5c1fed1bd2e8503f2c
Ashutosh Kumar <63195712+ashutosh-hep@users.noreply.github.com>
2025-03-17 03:23:30 +0530

    Update dockerhubAuth.groovy

--- vars/dockerhubAuth.groovy
+++ vars/dockerhubAuth.groovy
@@ -1,7 +1,7 @@
 def call(accesstoken, username) {
     // Use Groovy string interpolation to pass parameters into the shell command
     sh """
-        echo ${accesstoken} | docker login -u ${username} --password-stdin
+        echo ${accesstoken} | sudo docker login -u ${username} --password-stdin
     """
     
     // Print confirmation message
