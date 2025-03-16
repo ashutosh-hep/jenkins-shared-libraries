7d6094e4bfb40a92ade087820a2dceb48e9fd68e
Ashutosh Kumar <63195712+ashutosh-hep@users.noreply.github.com>
2025-03-17 02:51:55 +0530

    Update dockerhubAuth.groovy

--- vars/dockerhubAuth.groovy
+++ vars/dockerhubAuth.groovy
@@ -1,6 +1,6 @@
 def call(accesstoken,username) {
     sh '''
-        sudo docker login -u ${username} -p ${accesstoken}
+        sudo docker login -u ${username} --password-stdin ${accesstoken}
     '''
     echo "Authenctication successful"
 }
